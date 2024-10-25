220065c137c5425a353a2bff5244f5f2b3126f49
tanishqmalik <tanishqvideo@gmail.com>
2024-10-25 22:11:44 +0530

    okay

--- Yelpcamp-Project-MEN-EJS-STACK/app.js
+++ Yelpcamp-Project-MEN-EJS-STACK/app.js
@@ -4,6 +4,7 @@ const port = 3000;
 const path = require('path')
 const mongoose = require('mongoose');
 const Campground = require('./models/Campground');
+const { time } = require('console');
 
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp2')
@@ -18,6 +19,8 @@ mongoose.connect('mongodb://localhost:27017/yelp-camp2')
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
 app.get('/campgrounds', async (req, res)=>{
     const campgrounds = await Campground.find({})
@@ -28,12 +31,39 @@ app.get('/campgrounds/add', async(req,res)=>{
     res.render('Campgrounds/New')
 })
 
+app.post('/campgrounds', async (req,res)=>{
+    await Campground.create({
+        title: req.body.newtitle,
+        location: req.body.newloc
+    })
+    res.redirect('campgrounds')
+})
+
 app.get('/campgrounds/:id', async(req,res)=>{
     const {id} = req.params;
     const foundcampground = await Campground.findById(id)
 
     res.render('Campgrounds/Show', {foundcampground})
 })
+
+app.get('/campgrounds/update/:id', async(req,res)=>{
+
+    const {id} = req.params;
+
+    const foundcampground = await Campground.findById(id);
+    res.render('Campgrounds/Update', {foundcampground})
+})
+
+app.post('/campgrounds/:id', async (req,res)=>{
+    const {id} = req.params
+    await Campground.findByIdAndUpdate(id, {
+        title: req.body.title,
+        location: req.body.location
+    })
+
+    res.redirect('campgrounds')
+})
+
 
 
 
